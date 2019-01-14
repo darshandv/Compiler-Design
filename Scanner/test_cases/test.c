60e1dce87931d91233d7f0fbea2fef67f62b92c1
Darshan D V <darshandv10@gmail.com>
2019-01-15 00:24:06 +0530

    Symbol Table added

---
+++ Scanner/test_cases/test.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+  int x, y;
+  long long int total, diff;
+  int *ptr;
+  unsigned int a = 0x0f;
+  long int mylong = 123456l;
+  long int i, j;
+  for(i=0; i < 10; i++){
+    for(j=10; j > 0; j--){
+    printf("%d",i);
+    }
+  }
+  x = -10, y = 20;
+  x=x*3/2;
+  total = x + y;
+  diff = x - y;
+  int rem = x % y;
+  printf ("Total = %d \n", total);
+}
---
+++ Scanner/test_st.c
@@ -0,0 +1,13 @@
+#include "symbolTable.h"
+
+int main(int argc, char const *argv[])
+{
+	char *test = "while";
+	stEntry** symbol_table = new_table();
+	insert(symbol_table,test,100);
+	display(symbol_table);
+	insert(symbol_table,"if",150);
+	display(symbol_table);
+	return 0;
+}
+
