6f0e6284222257fe038f6078868ec31bd691b9e8
16CO216 <darshandv10@gmail.com>
2019-01-17 01:25:09 +0530

    Refactoring

--- Scanner/test_cases/test.c
+++ Scanner/test_cases/test.c
@@ -1,25 +1,10 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
 
-int main()
-{
-  int x, y;
-  long long int total, diff;
-  int *ptr;
-  unsigned int a = 0x0f;
-  unsigned int b = 0115;
-  unsigned int o = 01187;
-  long int mylong = 123456l;
-  long int i, j;
-  for(i=0; i < 10; i++){
-    for(j=10; j > 0; j--){
-    printf("%d",i);
-    }
-  }
-  x = -10, y = 20;
-  x=x*3/2;
-  total = x + y;
-  diff = x - y;
-  int rem = x % y;
-  printf ("Total = %d \n", total);
+// program to run a loop from 10 to 0
+int main() {
+	int i =10;
+	long long unsigned int b = 10;
+	/* this should be ignored*/while(i>=0) {
+		printf("Current value of i: %d", i); // print statement
+	}
 }
--- Scanner/test_cases/test_st.c
+++ Scanner/test_cases/test_st.c
@@ -1,17 +1,15 @@
 #include "symbolTable.h"
 
-
 /*
-** tHIS IS A TEST
-** SDSAD
-*/
+This is a test
+
+
 int main(int argc, char const *argv[])
 {
 	char *test = "while";
 	stEntry** symbol_table = new_table();
 	insert(symbol_table,test,100);
 	display(symbol_table);
-	insert(symbol_table,"if",150);
 	insert(symbol_table,"if",150);
 	display(symbol_table);
 	return 0;
