10bf47583555fe0a870231bd8919e42114b4e737
mahirjain25 <mahirjain25@gmail.com>
2019-01-17 01:36:10 +0530

    Test cases modified.

--- Scanner/test_cases/test.c
+++ Scanner/test_cases/test.c
@@ -7,4 +7,9 @@ int main() {
 	/* this should be ignored*/while(i>=0) {
 		printf("Current value of i: %d", i); // print statement
 	}
+	int a@b =5; //error
+    int a = 045; // octal
+    int b = 078; // error in octal
+    int c = 0x7b; // hexadecimal
+    int d = 0x6j; // should be an error
 }
