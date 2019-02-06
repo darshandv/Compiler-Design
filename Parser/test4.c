e7f90d45a311596c974181e03470ae12b2503539
mahirjain25 <mahirjain25@gmail.com>
2019-02-07 00:57:57 +0530

    stuff

---
+++ Parser/test4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a =5;
+    int b=3;
+    int c =2;
+    if(a) {
+        if(b){
+            c -=1;
+        }
+        else {
+            c *= 2;
+        }
+    }
+}
