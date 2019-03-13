c7461b87ea9963ed267059adabcbe577f50e2386
16CO216 <darshandv10@gmail.com>
2019-03-13 23:01:01 +0530

    Added a working test case

---
+++ Semantic_Analyser/OurTests/test_complete.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int add_something (){
+    int a = 10;
+    int b = 20;
+    int c;
+    c = a + b;
+    return c;
+}
+
+int condition(){
+    float d = 10.0;
+    float e = 40.0;
+    float f;
+    
+    if(d>f){
+        f = d * e;
+    }
+
+    return f;
+}
+
+int main(){
+    int p = 11;
+    {   
+        float k = 5.0;
+        while (p && k){
+            add_something();
+            condition();
+        }
+    }
+    return 0;
+}
